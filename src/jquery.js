b8a1c3643650d272dd9d3f13dfee10c84927fe0a
suixin <573505403@qq.com>
2020-07-06 21:26:01 +0800

    完成find函数

--- src/jquery.js
+++ src/jquery.js
@@ -1,6 +1,17 @@
-window.jQuery = function (selector) {
- const elements = document.querySelectorAll(selector)
- //api可以操作element
+window.jQuery = function (selectorOrArrayOrTemplate) {
+  let elements
+  if(typeof selectorOrArrayOrTemplate === 'string'){
+    if(selectorOrArrayOrTemplate[0] === '<'){
+      // 创建 div
+      elements=[createElement(selectorOrArrayOrTemplate)]
+    }else{
+      // 查找 div
+      elements = document.querySelectorAll(selectorOrArrayOrTemplate)
+    }
+  }else if(selectorOrArrayOrTemplate instanceof Array){
+    elements = selectorOrArrayOrTemplate
+  }
+  //api可以操作element
 // return api = {
 //   addClass(className){
 //    for (let i in Array.from(elements)) {//闭包
@@ -10,13 +21,27 @@ window.jQuery = function (selector) {
 //   }
 //  }
 // }
- return  {
-  addClass(className){
-   for (let i in Array.from(elements)) {//闭包
-    elements[i].classList.add(className)
-   }
-   return this
+  return {
+    //返回arr(数组)，就不能进行链式操作
+    find(selector) {
+      let arr = []
+      for (let i in Array.from(elements)) {
+        arr = arr.concat(Array.from(elements[i].querySelectorAll(selector)))
+      }
+      return jQuery(arr)
+    },
+
+
+
+
+    addClass(className) {
+      for (let i in Array.from(elements)) {//闭包
+        elements[i].classList.add(className)
+      }
+      return this
+    },
+
+
   }
- }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-jQuery('.test')//不返回元素们，而是返回api对象
-  .addClass('red')
-  .addClass('blue')//链式操作
-  .addClass('green')
+
+jQuery('.test1')
+  .find('.child')
+  .addClass('blue');
 
